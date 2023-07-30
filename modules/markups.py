dfa81bed97c2b983fd3aa07a6d5b6e8676c45d82
RubyRed <77550437+AndreyYad@users.noreply.github.com>
2023-07-31 01:49:15 +0300

    "murkup" на "markup"

--- modules/markups.py
+++ modules/markups.py
@@ -1,16 +1,27 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 def markup_start():
-    murkup = InlineKeyboardMarkup()
-    murkup.add(InlineKeyboardButton('Что предлагает «Яблоко»', callback_data='party_program_select'))
-    murkup.add(InlineKeyboardButton('Мой кандидат по округу', callback_data='z'))
-    murkup.add(InlineKeyboardButton('Регистрация избирателя', callback_data='z'))
-    murkup.add(InlineKeyboardButton('Я проголосовал(а)  ', callback_data='z'))
-    return murkup
+    markup = InlineKeyboardMarkup()
+    markup.add(InlineKeyboardButton('Что предлагает «Яблоко»', callback_data='party_program_select'))
+    markup.add(InlineKeyboardButton('Мой кандидат по округу', callback_data='z'))
+    markup.add(InlineKeyboardButton('Регистрация избирателя', callback_data='z'))
+    markup.add(InlineKeyboardButton('Я проголосовал(а)  ', callback_data='z'))
+    return markup
 
 def markup_party_program_select():
-    murkup = InlineKeyboardMarkup()
-    murkup.add(InlineKeyboardButton('Федеральная часть', callback_data='party_program'))
-    murkup.add(InlineKeyboardButton('Городская часть', callback_data='party_program'))
-    murkup.add(InlineKeyboardButton('🔙Назад', callback_data='start'))
-    return murkup
+    markup = InlineKeyboardMarkup()
+
+    btn1 = InlineKeyboardButton('Федеральная часть', callback_data='party_program')
+    btn2 = InlineKeyboardButton('Городская часть', callback_data='party_program')
+    btn3 = InlineKeyboardButton('🔙Назад', callback_data='start')
+
+    
+
+    markup.add(InlineKeyboardButton('Федеральная часть', callback_data='party_program'))
+    markup.add(InlineKeyboardButton('Городская часть', callback_data='party_program'))
+    markup.add(InlineKeyboardButton('🔙Назад', callback_data='start'))
+    return markup
+
+def markup_party_program_open():
+    markup = InlineKeyboardMarkup()
+    markup.add(InlineKeyboardButton('🔙Назад', callback_data='party_program'))
